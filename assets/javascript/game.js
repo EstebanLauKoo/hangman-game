db400c4945be27162aff8b49515829570575b16a
Esteban lau koo <Esteban lau koo>
2017-02-19 20:28:22 -0500

    v1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,23 +6,28 @@ var wins = 0;
     losses = 0,
     guessesLeft = 20
     guessesUsed = " "
+    computerGuesses = ""
 
 // When the user presses a key, it will run the following function...
 document.onkeyup = function(event) {
 
     // Determine which key was pressed
     var userGuess = event.key;
-        guessesUsed = guessesUsed + userGuess;
-    alert(userGuess);
+        guessesUsed = guessesUsed + " "  +  userGuess;
+
+    console.log(guessesUsed)
 
     // Sets the computerGuess variable equal to a random choice from the computerChoice array.
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     console.log(computerGuess)
+    alert("correct letter: " + computerGuess)
+    computerGuesses = computerGuesses + " " + computerGuess
 
     if (userGuess === computerGuess) {
         wins++
         guessesUsed = ""
         guessesLeft = 20
+        computerGuesses = ""
     }
     else {
         guessesLeft--
@@ -31,6 +36,7 @@ document.onkeyup = function(event) {
         losses++
         guessesLeft = 20
         guessesUsed = ""
+        computerGuesses = ""
     }
 
     console.log(losses)
@@ -41,7 +47,8 @@ document.onkeyup = function(event) {
         "<p>wins: " + wins + "</p>" +
         "<p>losses: " + losses + "</p>" +
         "<p>Guesses Left: " + guessesLeft + "</p>" +
-        "<p>Guesses Used: " + guessesUsed + "</p>";
+        "<p>Guesses Used: " + guessesUsed + "</p>" +
+        "<p>Computer Guesses: " + computerGuesses +"</p>" ;
 
     // Injecting the HTML we just created into our div and updating the game information on our page.
     document.querySelector("#game").innerHTML = html;
